e8b6a6595ae1a6a897c70eff437483e21a4b848d
ujjwal <ujjwalsharma515@gmail.com>
2024-03-09 20:48:26 +0530

    hi

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const generatejokes = async (q) => {
         humidity.innerHTML = data.current.humidity;
         cloud.innerHTML = data.current.cloud;
         pressure_mb.innerHTML = data.current.pressure_mb;
+		localtime.innerHTML=data.localtime;
 
 		
 	}
